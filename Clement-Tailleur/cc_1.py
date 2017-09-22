4f2b92606400cf1673a2987ebf5162f1b9c3f791
ClementTr <clement.tailleur@gmail.com>
2017-09-22 14:24:11 +0200

    Add Repo TD1

---
+++ Clement-Tailleur/cc_1.py
@@ -0,0 +1,109 @@
+import unittest
+
+
+# Given a string and a non-negative int n, return a larger string
+# that is n copies of the original string.
+
+def string_times(string, n):
+    return n*string
+
+# Given an array of ints, return True if one of the first 4 elements
+# in the array is a 9. The array length may be less than 4.
+def array_front9(nums):
+    #longueur=min(len(nums) & 4)
+    count = False
+    for n in nums[:3]:
+        if n == 9:
+            count = True
+    return count
+
+
+# Given a string, return the count of the number of times
+# that a substring length 2 appears  in the string and also as
+# the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+def last2(string):
+    c = 0
+    s1 = string[-2:]
+    s2 = string[:-2]
+    if s1 in s2:
+        c += 1
+    return c
+
+
+#Write a program that maps a list of words into a list of
+#integers representing the lengths of the correponding words.
+def length_words(array):
+    len_array = []
+    for a in array:
+        len_array.append(len(a))
+    return len_array
+
+#write fizbuzz programm
+def fizbuzz():
+    return
+
+#Write a function that takes a number and returns a list of its digits.
+def number2digits(number):
+    number = str(number)
+    decompose_number_list =  []
+    for n in number:
+         decompose_number_list.append(int(n))
+    return decompose_number_list
+
+#Write function that translates a text to Pig Latin and back.
+#English is translated to Pig Latin by taking the first letter of every word,
+#moving it to the end of the word and adding 'ay'
+def pigLatin(text):
+    text = text.lower()
+    new_text = ""
+    i = 0
+    for t in text.split():
+        if i == 0:
+            new_text += t[1].upper() + t[2:] + t[0] + "ay"
+            i = 1
+        else:
+            new_text += " " + t[1] + t[2:] + t[0] + "ay"
+    return new_text
+
+#Write a proramm that returna dictionary of occurences of the alphabet for a given string.
+# Test it with the Lorem upsuj
+#"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+def occurences(text):
+  return
+
+# Here's our "unit tests".
+class Lesson1Tests(unittest.TestCase):
+    fizbuzz()
+    def testArrayFront9(self):
+        self.assertEqual(array_front9([1, 2, 9, 3, 4]) , True)
+        self.assertEqual(array_front9([1, 2, 3, 4, 9]) , False)
+        self.assertEqual(array_front9([1, 2, 3, 4, 5]) , False)
+
+    def testStringTimes(self):
+        self.assertEqual(string_times('Hel', 2),'HelHel' )
+        self.assertEqual(string_times('Toto', 1),'Toto' )
+        self.assertEqual(string_times('P', 4),'PPPP' )
+
+    def testLast2(self):
+        self.assertEqual(last2('hixxhi') , 1)
+        self.assertEqual(last2('xaxxaxaxx') , 1)
+        self.assertEqual(last2('axxxaaxx') , 2)
+
+    def testLengthWord(self):
+        self.assertEqual(length_words(['hello','toto']) , [5,4])
+        self.assertEqual(length_words(['s','ss','59fk','flkj3']) , [1,2,4,5])
+
+    def testNumber2Digits(self):
+        self.assertEqual(number2digits(8849) , [8,8,4,9])
+        self.assertEqual(number2digits(4985098) , [4,9,8,5,0,9,8])
+
+    def testPigLatin(self):
+        self.assertEqual(pigLatin("The quick brown fox") , "Hetay uickqay rownbay oxfay")
+
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
