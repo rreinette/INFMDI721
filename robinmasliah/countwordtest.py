e3423dd4b668cf6e8805e20ceda3f0fc9a609f58
robinmasliah <robinmasliah@gmail.com>
2017-09-22 12:01:56 +0200

    Ajout fonction 2 count word

---
+++ robinmasliah/countwordtest.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python -tt
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+"""Wordcount exercise
+Google's Python class
+The main() below is already defined and complete. It calls print_words()
+and print_top() functions which you write.
+1. For the --count flag, implement a print_words(filename) function that counts
+how often each word appears in the text and prints:
+word1 count1
+word2 count2
+...
+Print the above list in order sorted by word (python will sort punctuation to
+come before letters -- that's fine). Store all the words as lowercase,
+so 'The' and 'the' count as the same word.
+
+
+2. For the --topcount flag, implement a print_top(filename) which is similar
+to print_words() but which prints just the top 20 most common words sorted
+so the most common word is first, then the next most common, and so on.
+Use str.split() (no arguments) to split on all whitespace.
+Workflow: don't build the whole program at once. Get it to an intermediate
+milestone and print your data structure and sys.exit(0).
+When that's working, try for the next milestone.
+Optional: define a helper function to avoid code duplication inside
+print_words() and print_top().
+"""
+
+
+
+def print_words(file):
+
+    wordcount={}
+    top_words = {}
+    
+    for word in file.read().split():
+        if word not in wordcount:
+            wordcount[word] = 1
+        else:
+            wordcount[word] += 1
+            
+
+    for k,v in wordcount.items():
+        print (k, v)
+        top_words = v
+    
+    print('test')
+    
+    for v in wordcount.items():
+        int(v)
+        if(v > 1):
+            print(top_words)
+        
+    file.close()
+    return(k, v);
+
+def print_top(file):
+    for word in file:
+       
+        return;
+        
+
+
+
+# This basic command line argument parsing code is provided and
+# calls the print_words() and print_top() functions which you must define.
+def main():
+    
+    print_words(file=open("C:\\Users\\Robin Masliah\\Desktop\\smalltxt.txt","r", encoding="utf-8-sig"))
+    
+    
+if __name__ == '__main__':
+  main()
