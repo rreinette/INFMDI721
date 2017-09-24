b229223be438d98f2bb9b96d50b589994a7c410e
baubert <baptiste.aubert.m@gmail.com>
2017-09-24 10:53:48 +0200

    cc1

--- baptiste-aubert/exo_cc_lesson1.py
+++ baptiste-aubert/exo_cc_lesson1.py
@@ -1,78 +1,88 @@
 import unittest
- #il le vim 
 
-# POUR RUNNER python filename 
-#Checher les sets
-#Checker zip map lambda
-#module unitest, SELFASSETEQUALNIANIANIA
-#UNITEST TOUJOURS on peut avoir un selfError!il faut essayer durtout avec les conditions limites
-#Check Test DRIVEN DEVELOPMENT
 
 # Given a string and a non-negative int n, return a larger string
 # that is n copies of the original string.
 
 def string_times(string, n):
-    return n*string
-#%%
+    return string * n
+
 # Given an array of ints, return True if one of the first 4 elements
-# in the array is a 9. The array length may be less than 4.
+# # in the array is a 9. The array length may be less than 4.
 def array_front9(nums):
-    #longueur=min(len(nums) & 4)
-    length = nums.length
-    for i in range(0,length):
-        if i <= 3 and nums[i] == 9:
+    for i in range(0,len(nums)):
+        if (i<=3 and nums[i]==9):
             return True
-        return False
-#%%
+    return False
 
-# Given a string, return the count of the number of times
-# that a substring length 2 appears  in the string and also as
-# the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+
+
+# # Given a string, return the count of the number of times
+# # that a substring length 2 appears  in the string and also as
+# # the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
 def last2(string):
-    if string.length < 2:
-        return 
-    pattern = string[-2:]
-    count = 0 
-    for i in range(0, string.length-3):
-        if string [i:i+2]==pattern:
+    look = string[-2:]
+    count =0
+    for i in range(0, len(string)-4, 1):
+        if (string[i:i+2]==look):
             count +=1
-    return 
+    return count
 
-#Write a program that maps a list of words into a list of
-#integers representing the lengths of the correponding words.
+
+
+
+# #Write a program that maps a list of words into a list of
+# #integers representing the lengths of the correponding words.
 def length_words(array):
-    map(niania lambda)
-    return
+    return list(map(lambda x : len(x), array))
 
-#write fizbuzz programm
+#
+# #write fizbuzz programm
 def fizbuzz():
-    return
+    result=[]
+    for i in range(0,100):
+        if i%3==0:
+            result.append("fizz")
+        elif i%5==0:
+            result.append("buzz")
+        else:
+            result.append(i)
+    print (result)
 
-#Write a function that takes a number and returns a list of its digits.
+
+
+# #Write a function that takes a number and returns a list of its digits.
 def number2digits(number):
-  return
+    text=str(number)
+    result=[]
+    for it in text:
+        result.append(int(it))
+    return result
 
-#Write function that translates a text to Pig Latin and back.
-#English is translated to Pig Latin by taking the first letter of every word,
-#moving it to the end of the word and adding 'ay'
+
+# #Write function that translates a text to Pig Latin and back.
+# #English is translated to Pig Latin by taking the first letter of every word,
+# #moving it to the end of the word and adding 'ay'
 def pigLatin(text):
-    ' '.join([ (x[1:] + x [0] + 'ay' for x in test.split(' ')])
-  return
+    return ' '.join(x[1:]+x[0]+ 'ay' for x in text.split(' ')).capitalize()
 
-#Write a proramm that returna dictionary of occurences of the alphabet for a given string.
-# Test it with the Lorem upsuj
-#"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+# #Write a proramm that returna dictionary of occurences of the alphabet for a given string.
+# # Test it with the Lorem upsuj
+# #"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 def occurences(text):
-    dictionnare = 'qwerty'
-    result = {}
-    [result[i] += 1 if result[i] else result[i] = for i in text]
-  return
+    to_ret = {}
+    for word in text.split(' '):
+        to_ret[word]=to_ret.get(word,0)+1
+    return to_ret
 
+print (occurences("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"))
+
+fizbuzz()
 # Here's our "unit tests".
 class Lesson1Tests(unittest.TestCase):
-    fizbuzz()
+
     def testArrayFront9(self):
-        self.assertEqual(array_front9([1, 2, 9, 3, 4]) , True)
+        self.assertEqual(array_front9([1, 2, 9, 5, 8]) , True)
         self.assertEqual(array_front9([1, 2, 3, 4, 9]) , False)
         self.assertEqual(array_front9([1, 2, 3, 4, 5]) , False)
 
@@ -104,4 +114,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
