518f755fecab6e81c6e5ccca00c2f6e1dde80815
Alexandre Lenoir <xhelas@yahoo.fr>
2017-09-24 20:46:07 +0200

    Change to new script names to cope with shared "telecom-paristech-ms-bgd-2018/INFMDI721" github repository  repository

--- alexandre-lenoir/Lesson1/exo_cc_lesson_1.py
+++ alexandre-lenoir/Lesson1/exo_cc_lesson_1.py
@@ -21,18 +21,17 @@ def array_front9(nums):
 # that a substring length 2 appears  in the string and also as
 # the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
 def last2(string):
-    last = string[-2:]
-    pos = 0
+    if len(string) < 2:
+        return 0
+    last2chars = string[-2:]
     count = 0
-    while string.find(last,pos) != -1 and string.find(last,pos) != -2:
-        if pos == string.find(last,pos):
-            count += 1
-            pos += 1
+    for pos in range(len(string)-2):
+        count += string[pos:pos+2] == last2chars
     return count
 
 
 #Write a program that maps a list of words into a list of
-#integers representing the lengths of the correponding words.
+#integers representing the lengths of the corresponding words.
 def length_words(array):
     lengths = []
     for index in range(len(array)):
@@ -41,7 +40,7 @@ def length_words(array):
 
 #write fizbuzz programm
 def fizbuzz():
-    return
+    print(" ".join([("Fizz" if ( i%3 == 0 ) else "") + "Buzz" * ( i%5 == 0 ) or str(i) for i in range(1,101)]))
 
 #Write a function that takes a number and returns a list of its digits.
 def number2digits(number):
@@ -55,13 +54,20 @@ def number2digits(number):
 #English is translated to Pig Latin by taking the first letter of every word,
 #moving it to the end of the word and adding 'ay'
 def pigLatin(text):
-    return
+    return " ".join([word[1:] + word[0] + "ay" for word in text.split()]).capitalize()
 
-#Write a proramm that returna dictionary of occurences of the alphabet for a given string.
+#Write a program that return a dictionary of occurences of the alphabet for a given string.
 # Test it with the Lorem upsuj
 #"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 def occurences(text):
-  return
+    dictionary = {}
+    for letter in text.lower():
+        if letter.isalpha():
+            if letter in dictionary:
+                dictionary[letter] += 1
+            else:
+                dictionary[letter] = 1
+    return dictionary
 
 # Here's our "unit tests".
 class Lesson1Tests(unittest.TestCase):
@@ -78,6 +84,7 @@ class Lesson1Tests(unittest.TestCase):
 
     def testLast2(self):
         self.assertEqual(last2('hixxhi') , 1)
+        self.assertEqual(last2('iixiii') , 2)
         self.assertEqual(last2('xaxxaxaxx') , 1)
         self.assertEqual(last2('axxxaaxx') , 2)
 
@@ -91,6 +98,9 @@ class Lesson1Tests(unittest.TestCase):
 
     def testPigLatin(self):
         self.assertEqual(pigLatin("The quick brown fox") , "Hetay uickqay rownbay oxfay")
+        
+    def testOccurences(self):
+        self.assertEqual(occurences("Hello is like hi!"), {'h':2,'l':3,'e':2,'o':1,'i':3,'k':1,'s':1})
 
 
 
@@ -98,5 +108,9 @@ def main():
     unittest.main()
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except:
+        pass
+    stop=True
 
